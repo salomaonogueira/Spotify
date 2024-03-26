9fb84c360a3f6de0710572328a7e0ceb6837e444
Salomão David <156952876+salomaonogueira@users.noreply.github.com>
2024-03-26 17:27:34 -0400

    Add files via upload

---
+++ spotify-imersao-develop/script.js
@@ -0,0 +1,45 @@
+//BOM DIA | BOA TARDE | BOA NOITE
+
+// Obtém a referência do elemento com o ID "greeting"
+const greetingElement = document.getElementById("greeting");
+
+// Obtém a hora atual do sistema
+const currentHour = new Date().getHours();
+
+// Define a saudação com base na hora atual - Forma mais simples
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+    document.addEventListener('input', function() {
+      const searchTerm = searchInput.value.toLowerCase();
+      if (searchTerm === '') {
+        playlistContainer.classList.add("hidden");
+        resultArtist.classList.remove('hidden');
+        return;
+        // Faça algo quando o campo de busca estiver vazio
+    
+        requestApi(searchInput);
+      }
+    });
+
+greetingElement.textContent = greetingMessage;
+
+// GRID INTELIGENTE
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
+  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
+
+  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+  console.log({ container });
+  console.log({ numColumns });
+});
+//observando a mudança do elemento
+observer.observe(container);
